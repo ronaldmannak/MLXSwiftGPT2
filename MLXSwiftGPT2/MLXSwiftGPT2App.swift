226080068fbb253f8c14ea4a5023e3fcca35e86b
Ronald Mannak <ronaldmannak@me.com>
2024-09-02 15:50:33 -0700

    Initial Commit

---
+++ MLXSwiftGPT2/MLXSwiftGPT2App.swift
@@ -0,0 +1,17 @@
+//
+//  MLXSwiftGPT2App.swift
+//  MLXSwiftGPT2
+//
+//  Created by Ronald Mannak on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct MLXSwiftGPT2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
